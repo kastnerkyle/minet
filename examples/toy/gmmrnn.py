Bug fixes and a WIP commit for full covariance cost with binary channels

examples/toy/gmmrnn.py
@@ -6,18 +6,18 @@ import matplotlib.pyplot as plt
 
 n_h = 50
 time_steps = 100
-# n_y is equal to the number of classes
 random_state = np.random.RandomState(1999)
 
-sin = np.sin(np.linspace(-3 * np.pi, 3 * np.pi, time_steps))
-seq = sin[None, :, None]
+sin = np.sin(np.linspace(-3 * np.pi, 3 * np.pi, time_steps))[:, None]
+cos = np.cos(np.linspace(-3 * np.pi, 3 * np.pi, time_steps))[:, None]
+seq = np.concatenate((sin, cos), axis=-1)[None]
 
 clf = GMMRNN(learning_alg="rmsprop", n_mixture_components=20,
              hidden_layer_sizes=[n_h],
              max_iter=1000, learning_rate=.0001,
              bidirectional=False, momentum=0.99,
              recurrent_activation="lstm", minibatch_size=10,
-             save_frequency=500, random_seed=1999)
+             save_frequency=1000, random_seed=1999)
 
 shp = seq.shape
 seq_r = seq.reshape(shp[0] * shp[1], shp[-1])

minet/layers.py
@@ -320,8 +320,7 @@ def build_recurrent_conditional_lstm_layer_from_params(params, input_variable,
     return rval[:4], params
 
 
-def init_recurrent_lstm_layer(input_size, hidden_size, output_size,
-                              random_state):
+def init_recurrent_lstm_layer(input_size, hidden_size, random_state):
     # input to LSTM
     W_ = np.concatenate(
         [np_rand((input_size, hidden_size), random_state),
@@ -349,11 +348,9 @@ def init_recurrent_lstm_layer(input_size, hidden_size, output_size,
     return params
 
 
-def build_recurrent_lstm_layer(input_size, hidden_size, output_size,
-                               input_variable, mask,
+def build_recurrent_lstm_layer(input_size, hidden_size, input_variable, mask,
                                random_state, one_step=False):
-    params = init_recurrent_lstm_layer(input_size, hidden_size, output_size,
-                                       random_state)
+    params = init_recurrent_lstm_layer(input_size, hidden_size, random_state)
     return build_recurrent_lstm_layer_from_params(params, input_variable, mask,
                                                   random_state,
                                                   one_step=one_step)
@@ -395,8 +392,8 @@ def build_recurrent_lstm_layer_from_params(params, input_variable, mask,
     # https://github.com/Theano/Theano/issues/1772
     init_hidden = T.zeros((n_samples, hidden_size))
     init_cell = T.zeros((n_samples, hidden_size))
-    #init_hidden = T.unbroadcast(init_hidden, 0)
-    #init_cell = T.unbroadcast(init_cell, 0)
+    init_hidden = T.unbroadcast(init_hidden, 0)
+    init_cell = T.unbroadcast(init_cell, 0)
 
     x = T.dot(input_variable, W) + b
     if one_step: